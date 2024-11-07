a2860231ac9e7900a5928dd8e2bc834818e4027f
Im-Mohsen <mohsendarabi2003@yahoo.com>
2024-11-07 16:13:46 +0330

    add product list template

--- shop/urls.py
+++ shop/urls.py
@@ -6,5 +6,5 @@ app_name = 'shop'
 urlpatterns = [
     path('products', views.product_list, name='products_list'),
     path('products/<slug:category_slug>', views.product_list, name='products_list_by_category'),
-    path('product/<int:id>/<slug:slug', views.product_detail, name='product_detail'),
+    path('product/<int:id>/<slug:slug>', views.product_detail, name='product_detail'),
 ]
