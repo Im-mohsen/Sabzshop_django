2d7421a78682bff19af0be8281b2d74b5f4d051c
Im-Mohsen <mohsendarabi2003@yahoo.com>
2024-12-07 23:42:08 +0330

    Create first api with ListAPIView & RetrieveAPIView

--- Sabzshop/settings.py
+++ Sabzshop/settings.py
@@ -164,6 +164,15 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ],
+    # دیفالت وجود دارد صرفا برای نیاز
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ],
+    # ------------------------
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'shop'
+app_name = 'api'
 
 urlpatterns = [
-
+    path('products/', views.ProductListApiView.as_view(), name='products_list_api'),
+    path('product/<int:pk>/', views.ProductDetailApiView.as_view(), name='product_detail_api'),
 ]
