23846a0a92e58695193ae3e8c823d2b2cd66d6be
Im-Mohsen <mohsendarabi2003@yahoo.com>
2024-12-07 23:57:18 +0330

    add nested serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from shop.models import Product
+from shop.models import Product, ProductFeatured
+
+
+class ProductFeaturesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductFeatured
+        fields = ['name', 'value']
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    features = ProductFeaturesSerializer(many=True, read_only=True)
+
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'new_price']
+        fields = ['id', 'name', 'description', 'new_price', 'features']
